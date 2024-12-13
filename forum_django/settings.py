19ea7eb872fcbd359fe1a828e4dd8200bc78c0e8
anastasiakr <anastasiia.lipatova@gmail.com>
2024-12-13 11:51:51 -0500

    [feat] added DB condition in settings.py, created a .env.example file

--- forum_django/settings.py
+++ forum_django/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from decouple import config
 
+IS_SERVER = config('IS_SERVER') == '1'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
@@ -79,12 +81,27 @@ WSGI_APPLICATION = 'forum_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# We're running on the server so use postgres
+if IS_SERVER:
+    # Connects to a Postgres database using credentials stored in Env Vars
+    DATABASES = {
+        "default": {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'HOST': config('DB_HOST'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'PORT': '5432',
+        }
     }
-}
+# We're running locally so use sqlite3
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
