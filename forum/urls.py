b3206d55cef5f8d7650ee39edc15d369b9b9e84f
anastasiakr <anastasiia.lipatova@gmail.com>
2024-12-17 11:32:36 -0500

    [feat] added category_list paths to urls

--- forum/urls.py
+++ forum/urls.py
@@ -8,4 +8,5 @@ app_name = 'forum'
 urlpatterns = [
     # dashboard
     path('', views.get_dashboard, name='dashboard'),
-    ]
+    path('categories/', views.CategoryList.as_view(), name="category_list"),
+]
